643a6f3b7ba6fad42f21260a0cd44f5059adf36c
Denys Tereshchuk <terrya@ukr.net>
2023-01-26 19:00:50 +0400

    Added position, first name, last name for Employee in admin panel

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from cafe.models import Position, Employee, DishType, Ingredient, Dish, Order
 
 admin.site.register(Position)
-admin.site.register(Employee)
 admin.site.register(DishType)
 admin.site.register(Ingredient)
 admin.site.register(Dish)
 admin.site.register(Order)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Staff", {"fields": ("position",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "email",
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
