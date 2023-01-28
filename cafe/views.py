349e92430905366ebd3aa2897cbf2fbf5105809a
Denys Tereshchuk <terrya@ukr.net>
2023-01-28 23:29:35 +0400

    Added index in views

--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cafe.models import Dish
+
+
+def index(request):
+    context = {
+        "num_sushi": Dish.objects.filter(dish_type__name="sushi").count(),
+        "num_salad": Dish.objects.filter(dish_type__name="salad").count(),
+        "num_pizza": Dish.objects.filter(dish_type__name="pizza").count(),
+        "num_soup": Dish.objects.filter(dish_type__name="soup").count()
+    }
+    return render(request, "cafe/index.html", context=context)
