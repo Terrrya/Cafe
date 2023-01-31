574db43f5d05236e09a5765584a428d3b5147c11
Denys Tereshchuk <terrya@ukr.net>
2023-01-31 23:56:22 +0400

    Changed index to home views

--- cafe/views.py
+++ cafe/views.py
@@ -6,14 +6,25 @@ from cafe.models import Dish, Position, DishType, Ingredient, Employee, Order
 from django.urls import reverse_lazy
 
 
-def index(request):
+# def index(request):
+#     context = {
+#         "num_sushi": Dish.objects.filter(dish_type__name="sushi").count(),
+#         "num_salad": Dish.objects.filter(dish_type__name="salad").count(),
+#         "num_pizza": Dish.objects.filter(dish_type__name="pizza").count(),
+#         "num_soup": Dish.objects.filter(dish_type__name="soup").count()
+#     }
+#     return render(request, "cafe/index.html", context=context)
+
+
+def home(request):
     context = {
+        "orders": Order.objects.filter(employee=request.user),
         "num_sushi": Dish.objects.filter(dish_type__name="sushi").count(),
         "num_salad": Dish.objects.filter(dish_type__name="salad").count(),
         "num_pizza": Dish.objects.filter(dish_type__name="pizza").count(),
         "num_soup": Dish.objects.filter(dish_type__name="soup").count()
     }
-    return render(request, "cafe/index.html", context=context)
+    return render(request, "cafe/home.html", context=context)
 
 
 class PositionCreateView(generic.CreateView):
