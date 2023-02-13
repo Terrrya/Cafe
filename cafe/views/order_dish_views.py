d56d03ad4d69a4a3a57caa84c4907192764f6b90
Denys Tereshchuk <terrya@ukr.net>
2023-02-13 13:48:31 +0400

    Added validate when dishes add in order

--- cafe/views/order_dish_views.py
+++ cafe/views/order_dish_views.py
@@ -1,6 +1,10 @@
 from django.views import generic
-from cafe.models import Order, OrderDish, Dish
+from cafe.models import Order, OrderDish, Dish, Recipe, Ingredient
 from django.urls import reverse_lazy
+from django.core.exceptions import ValidationError
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 
 class OrderDishCreateView(generic.CreateView):
@@ -10,6 +14,9 @@ class OrderDishCreateView(generic.CreateView):
 
     def form_valid(self, form):
         form_fields = form.save(commit=False)
+        if form_fields.amount <= 0:
+            messages.warning(self.request, "You should check at least 1 pc of dishes")
+            return HttpResponseRedirect(self.get_success_url())
         form_fields.order = Order.objects.get(id=self.kwargs["pk"])
         form_fields.dish = Dish.objects.get(name=self.request.POST["dish"])
         form_fields.save()
