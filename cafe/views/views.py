8b2a2d668cd2b1acc6988e064d4a6ad00cb226b6
Denys Tereshchuk <terrya@ukr.net>
2023-02-16 17:43:42 +0400

    Added home templates

--- cafe/views/views.py
+++ cafe/views/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
-
 from cafe.forms import SearchForm
 from cafe.models import Dish, Order
 from django.views import generic
-
-# def index(request):
-#     context = {
-#         "num_sushi": Dish.objects.filter(dish_type__name="sushi").count(),
-#         "num_salad": Dish.objects.filter(dish_type__name="salad").count(),
-#         "num_pizza": Dish.objects.filter(dish_type__name="pizza").count(),
-#         "num_soup": Dish.objects.filter(dish_type__name="soup").count()
-#     }
-#     return render(request, "cafe/index.html", context=context)
+from django.utils import timezone
 
 
 def home(request):
+    popular_dish = Dish.objects.first()
+    unpopular_dish = popular_dish
+    popular_count = popular_dish.orders.count()
+    unpopular_count = popular_count
+    for dish in Dish.objects.all():
+        popularity = dish.orders.count()
+        if popular_count < popularity:
+            popular_dish = dish
+            popular_count = popularity
+        if unpopular_count > popularity:
+            unpopular_count = popularity
+            unpopular_dish = dish
+    _, week, _ = timezone.now().isocalendar()
+    week_orders = (Order.objects.filter(
+        created_at__week=week
+    ))
+    week_income = sum(order.total_price for order in week_orders)
+    today_orders = (Order.objects.filter(created_at__day=timezone.now().day))
+    today_income = sum(order.total_price for order in today_orders)
+    month_orders = (Order.objects.filter(
+        created_at__month=timezone.now().month
+    ))
+    month_income = sum(order.total_price for order in month_orders)
     context = {
-        "orders": Order.objects.filter(employee=request.user),
+        "unpopular_dish": unpopular_dish,
+        "last_month_income": month_income,
+        "today_income": today_income,
+        "last_week_income": week_income,
+        "popular_dish": popular_dish,
         "num_sushi": Dish.objects.filter(dish_type__name="sushi").count(),
         "num_salad": Dish.objects.filter(dish_type__name="salad").count(),
         "num_pizza": Dish.objects.filter(dish_type__name="pizza").count(),
@@ -26,7 +44,6 @@ def home(request):
 
 
 class UniversalListView(generic.ListView):
-    # paginate_by = 3
     key_to_search = ""
 
     def get_context_data(self, *, object_list=None, **kwargs):
