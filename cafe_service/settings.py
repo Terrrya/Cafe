f2e734a1f3f48a771c2764f31c8dfd33cd117a28
Denys Tereshchuk <terrya@ukr.net>
2023-02-22 00:24:21 +0400

    Added condition for AWS storage

--- cafe_service/settings.py
+++ cafe_service/settings.py
@@ -165,18 +165,20 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # AWS S3 Bucket Configurations
 
-AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+if not DEBUG:
 
-AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 
-AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 
-AWS_S3_FILE_OVERWRITE = False
+    AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
 
-AWS_DEFAULT_ACL = None
+    AWS_S3_FILE_OVERWRITE = False
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_DEFAULT_ACL = None
 
-AWS_S3_REGION_NAME = "eu-central-1"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-AWS_S3_ADDRESSING_STYLE = "virtual"
+    AWS_S3_REGION_NAME = "eu-central-1"
+
+    AWS_S3_ADDRESSING_STYLE = "virtual"
