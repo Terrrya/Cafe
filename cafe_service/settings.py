1efce2290b4e425d220a0ab0dfff5e85abce3b0e
Denys Tereshchuk <terrya@ukr.net>
2023-02-21 17:22:54 +0400

    Removed insecure Django secret keyfrom settings

--- cafe_service/settings.py
+++ cafe_service/settings.py
@@ -23,10 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-=gfx8*zcs^h(0nqnu(jyw3qy!#opvc#=2_jn=mq_5a4n=6@_a%"
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
