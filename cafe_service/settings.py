7d70180caca5af06b856a59bcabbfb8d2ef76cd4
Denys Tereshchuk <terrya@ukr.net>
2023-02-24 02:17:59 +0400

    Added settings for no autoload fixture in TEST database in settings.py

--- cafe_service/settings.py
+++ cafe_service/settings.py
@@ -99,6 +99,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": "test_db.sqlite3",
+            "MIGRATE": False,
+        }
     }
 }
 
