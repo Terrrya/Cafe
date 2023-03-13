5419e64edbab839db74bf3d05cf15d418caf20cf
Denys Tereshchuk <terrya@ukr.net>
2023-03-13 13:48:22 +0400

    Updated filter in orders on home page

--- cafe/views/home.py
+++ cafe/views/home.py
@@ -1,4 +1,5 @@
 import calendar
+from datetime import datetime
 from decimal import Decimal
 
 from django.contrib.auth.decorators import login_required
@@ -16,14 +17,22 @@ class Calendar(LoginRequiredMixin, calendar.HTMLCalendar):
             return f"<td class='{self.cssclass_noday}'>&nbsp;</td>"
         else:
             if day == timezone.localdate(timezone.now()).day:
-                return f"<td class='{self.cssclasses[weekday]} " \
-                       f"today'>{day}</td>"
+                return (
+                    f"<td class='{self.cssclasses[weekday]} "
+                    f"today'>{day}</td>"
+                )
             return f"<td class='{self.cssclasses[weekday]}'>{day}</td>"
 
 
-def order_total_price_income(key: str, value: int) -> Decimal:
-    orders = Order.objects.filter(**{f"created_at__{key}": value})
-    return sum(order.total_price for order in orders)
+def order_total_price_income(
+    value: int | datetime, key: str = None
+) -> Decimal:
+    if key:
+        orders = Order.objects.filter(**{f"created_at__{key}": value})
+        return sum(order.total_price for order in orders)
+    return sum(
+        order.total_price for order in Order.objects.filter(created_at=value)
+    )
 
 
 @login_required
@@ -48,11 +57,9 @@ def home(request: HttpRequest) -> HttpResponse:
     year, week, _ = timezone.localdate(timezone.now()).isocalendar()
     month = timezone.localdate(timezone.now()).month
     cal = Calendar().formatmonth(theyear=year, themonth=month)
-    week_income = order_total_price_income("week", week)
-    today_income = order_total_price_income(
-        "day", timezone.localdate(timezone.now()).day
-    )
-    month_income = order_total_price_income("month", month)
+    week_income = order_total_price_income(key="week", value=week)
+    today_income = order_total_price_income(timezone.localdate(timezone.now()))
+    month_income = order_total_price_income(key="month", value=month)
     context = {
         "cal": cal,
         "unpopular_dish": unpopular_dish,
